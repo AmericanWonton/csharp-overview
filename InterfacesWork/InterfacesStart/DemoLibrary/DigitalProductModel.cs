2863fce88d590a7bb54abb147409e1dd83323bef
missusdever <missusMGdess@gmail.com>
2021-09-23 18:43:15 -0500

    Added a folder to learn about interfaces.

---
+++ InterfacesWork/InterfacesStart/DemoLibrary/DigitalProductModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class DigitalProductModel : IProductModel
+    {
+        public string Title { get; set; }
+
+        public bool HasOrderBeenCompleted { get => throw new NotImplementedException(); }
+
+        public void ShipItem(CustomerModel customer)
+        {
+                Console.WriteLine($"Simulating emailing { Title } to { customer.EmailAddress }");
+        }
+    }
+}
