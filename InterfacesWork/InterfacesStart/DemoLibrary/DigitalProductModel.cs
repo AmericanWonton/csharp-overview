90ec2857c704f8d11d4e1ddab896f9945f5cda00
missusdever <missusMGdess@gmail.com>
2021-09-23 19:26:59 -0500

    Almost done with lesson.

--- InterfacesWork/InterfacesStart/DemoLibrary/DigitalProductModel.cs
+++ InterfacesWork/InterfacesStart/DemoLibrary/DigitalProductModel.cs
@@ -6,15 +6,27 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    public class DigitalProductModel : IProductModel
+    public class DigitalProductModel :  IDigitalProductModel
     {
         public string Title { get; set; }
 
-        public bool HasOrderBeenCompleted { get => throw new NotImplementedException(); }
+        public bool HasOrderBeenCompleted { get; private set; }
+
+        public int TotalDownloadsLeft { get; private set; } = 5;
 
         public void ShipItem(CustomerModel customer)
         {
+            if (HasOrderBeenCompleted == false)
+            {
                 Console.WriteLine($"Simulating emailing { Title } to { customer.EmailAddress }");
+                TotalDownloadsLeft -= 1;
+                if (TotalDownloadsLeft < 1)
+                {
+                    //Used to check against negative downloads
+                    HasOrderBeenCompleted = true;
+                    TotalDownloadsLeft = 0;
+                }
+            }
         }
     }
 }
